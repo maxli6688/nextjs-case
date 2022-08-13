8e25a00578093b94ed75c631c1ca51ab870a9c83
enjoyali <2810539286@qq.com>
2022-08-13 12:12:58 +0800

    fix: config tailwindcss

---
+++ pages/404.tsx
@@ -0,0 +1,12 @@
+export default function Custom404() {
+  return (
+    <div
+      className="w-full h-full flex flex-col items-center justify-center"
+      style={{
+        minHeight: `40vh`,
+      }}
+    >
+      <h1>404 - Page Not Found</h1>
+    </div>
+  );
+}
---
+++ pages/500.tsx
@@ -0,0 +1,12 @@
+export default function Custom500() {
+  return (
+    <div
+      className="w-full h-full flex flex-col items-center justify-center"
+      style={{
+        minHeight: `40vh`,
+      }}
+    >
+      <h1>500 - Server-side error occurred</h1>
+    </div>
+  );
+}
